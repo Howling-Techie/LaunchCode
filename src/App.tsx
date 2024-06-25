e406f9825cf7c6c7c473c0a7484394ad27dd2f40
Howling Techie <howling.techie@gmail.com>
2024-06-25 12:55:14 +0100

    Click and drag us text was breaking formatting

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ import {
 import {
     arrayMove, rectSortingStrategy,
     SortableContext,
-    sortableKeyboardCoordinates,
+    sortableKeyboardCoordinates, verticalListSortingStrategy,
 } from "@dnd-kit/sortable";
 import {VscRocket} from "react-icons/vsc";
 
@@ -181,43 +181,84 @@ function App() {
                 </section>
                 <section className="w-full mb-16">
                     <h2 className="text-4xl font-bold text-[#FF204E] mb-6 text-center">Services</h2>
-                    <h3 className="text-white rotate-6 translate-x-1/2 -translate-y-16 md:-translate-y-8">
-                        Click and Drag Us!</h3>
-                    <DndContext
-                        sensors={sensors}
-                        collisionDetection={closestCenter}
-                        onDragEnd={handleDragEnd}
-                    >
-                        <SortableContext
-                            items={services}
-                            strategy={rectSortingStrategy}
+                    <div className="w-full flex justify-center">
+                        <h3 className="absolute text-white text-sm rotate-12 w-48 text-center -translate-y-16 translate-x-1/2">
+                            Click and Drag Us!</h3>
+                    </div>
+                    <div className="hidden md:inline">
+                        <DndContext
+                            sensors={sensors}
+                            collisionDetection={closestCenter}
+                            onDragEnd={handleDragEnd}
                         >
-                            <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-                                {services.map(item => <ServiceItem key={item.id} id={item.id}>
-                                    <div
-                                        className="min-h-full group service-item bg-white text-black p-6 rounded-lg shadow-lg hover:shadow-2xl duration-750 transition transform hover:scale-105 flex flex-col items-center overflow-hidden"
-                                        aria-label={item.title}>
+                            <SortableContext
+                                items={services}
+                                strategy={rectSortingStrategy}
+                            >
+                                <div className="grid grid-cols-3 gap-8">
+                                    {services.map(item => <ServiceItem key={item.id} id={item.id}>
                                         <div
-                                            className="absolute inset-0 bg-gradient-to-r from-[#FF204E] to-[#FF5A5E] transform -translate-x-full group-active:translate-x-0 transition-transform duration-500 ease-in-out overflow-hidden z-10">
+                                            className="min-h-full group service-item bg-white text-black p-6 rounded-lg shadow-lg hover:shadow-2xl duration-750 transition transform hover:scale-105 flex flex-col items-center overflow-hidden"
+                                            aria-label={item.title}>
                                             <div
-                                                className="absolute z-10 flex flex-col items-center text-white duration-500 ease-in-out translate-x-full group-active:translate-x-0 py-6 px-6 w-full">
-                                                <item.icon className="text-6xl mb-4 transition"/>
-                                                <h3 className="text-center text-2xl font-semibold mb-2 transition">{item.title}</h3>
-                                                <p className="text-center transition">{item.description}</p>
+                                                className="absolute inset-0 bg-gradient-to-r from-[#FF204E] to-[#FF5A5E] transform -translate-x-full group-active:translate-x-0 transition-transform duration-500 ease-in-out overflow-hidden z-10">
+                                                <div
+                                                    className="absolute z-10 flex flex-col items-center text-white duration-500 ease-in-out translate-x-full group-active:translate-x-0 py-6 px-6 w-full">
+                                                    <item.icon className="text-6xl mb-4 transition"/>
+                                                    <h3 className="text-center text-2xl font-semibold mb-2 transition">{item.title}</h3>
+                                                    <p className="text-center transition">{item.description}</p>
+                                                </div>
+                                            </div>
+                                            <div
+                                                className="flex flex-col items-center text-black w-full">
+                                                <item.icon className="text-[#FF204E] text-6xl mb-4"
+                                                           aria-hidden="true"/>
+                                                <h3 className="text-center text-2xl font-semibold mb-2 bg-gradient-to-r from-[#FF204E] to-[#FF5A5E] bg-clip-text text-transparent">{item.title}</h3>
+                                                <p className="text-center">{item.description}</p>
                                             </div>
                                         </div>
+                                    </ServiceItem>)}
+                                </div>
+                            </SortableContext>
+                        </DndContext>
+                    </div>
+                    <div className="md:hidden">
+                        <DndContext
+                            sensors={sensors}
+                            collisionDetection={closestCenter}
+                            onDragEnd={handleDragEnd}
+                        >
+                            <SortableContext
+                                items={services}
+                                strategy={verticalListSortingStrategy}
+                            >
+                                <div className="grid grid-cols-1 gap-8">
+                                    {services.map(item => <ServiceItem key={item.id} id={item.id}>
                                         <div
-                                            className="flex flex-col items-center text-black w-full">
-                                            <item.icon className="text-[#FF204E] text-6xl mb-4"
-                                                       aria-hidden="true"/>
-                                            <h3 className="text-center text-2xl font-semibold mb-2 bg-gradient-to-r from-[#FF204E] to-[#FF5A5E] bg-clip-text text-transparent">{item.title}</h3>
-                                            <p className="text-center">{item.description}</p>
+                                            className="min-h-full group service-item bg-white text-black p-6 rounded-lg shadow-lg hover:shadow-2xl duration-750 transition transform hover:scale-105 flex flex-col items-center overflow-hidden"
+                                            aria-label={item.title}>
+                                            <div
+                                                className="absolute inset-0 bg-gradient-to-r from-[#FF204E] to-[#FF5A5E] transform -translate-x-full group-active:translate-x-0 transition-transform duration-500 ease-in-out overflow-hidden z-10">
+                                                <div
+                                                    className="absolute z-10 flex flex-col items-center text-white duration-500 ease-in-out translate-x-full group-active:translate-x-0 py-6 px-6 w-full">
+                                                    <item.icon className="text-6xl mb-4 transition"/>
+                                                    <h3 className="text-center text-2xl font-semibold mb-2 transition">{item.title}</h3>
+                                                    <p className="text-center transition">{item.description}</p>
+                                                </div>
+                                            </div>
+                                            <div
+                                                className="flex flex-col items-center text-black w-full">
+                                                <item.icon className="text-[#FF204E] text-6xl mb-4"
+                                                           aria-hidden="true"/>
+                                                <h3 className="text-center text-2xl font-semibold mb-2 bg-gradient-to-r from-[#FF204E] to-[#FF5A5E] bg-clip-text text-transparent">{item.title}</h3>
+                                                <p className="text-center">{item.description}</p>
+                                            </div>
                                         </div>
-                                    </div>
-                                </ServiceItem>)}
-                            </div>
-                        </SortableContext>
-                    </DndContext>
+                                    </ServiceItem>)}
+                                </div>
+                            </SortableContext>
+                        </DndContext>
+                    </div>
                 </section>
                 <section className="w-full mb-16">
                     <h2 className="text-4xl font-bold text-[#FF204E] mb-6 text-center">Portfolio</h2>
